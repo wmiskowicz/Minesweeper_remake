48b307d3c70d242dde2877ad2461c63921f8d8d5
wmiskowicz <wojtek.miskowicz@gmail.com>
2025-02-09 22:01:17 +0100

    Cleaned up vga_package. Change vga_timing coding style to simple.

--- rtl/vga_pkg.sv
+++ rtl/vga_pkg.sv
@@ -1,46 +1,52 @@
 /**
- * Copyright (C) 2023  AGH University of Science and Technology
- * MTM UEC2 Projekt
  * Author: Wojciech Miskowicz
  * 
  * Description:
- * Package with vga related constants.
+ * Package with vga related constants. When changing the resolution update the upper
+ * section of this file with constants in the standard documentation.
+ * For more see:
+ * https://ez.analog.com/cfs-file/__key/communityserver-discussions-components-files/331/vesa_5F00_dmt_5F00_1.12.pdf
  */
 
 package vga_pkg;
 
 // Parameters for VGA Display 1440 x 900 @ 60fps using a 88.75 MHz clock;
+// Update this section for changing display resolution.
+
 localparam HOR_PIXELS = 1440;
 localparam VER_PIXELS = 900;
 
-localparam HCOUNT_MAX = 1599;
-localparam VCOUNT_MAX = 925;
+localparam HOR_TOTAL_TIME = 1600;
+localparam VER_TOTAL_TIME = 926;
 
+localparam HOR_BLANK_START = 1440;
+localparam HOR_BLANK_TIME  = 160;
 
+localparam HOR_SYNC_START = 1488;
+localparam HOR_SYNC_TIME  = 32;
 
-localparam HBLNK_START_FRONT = 1439;
-localparam HBLNK_STOP_FRONT = 1599;
+localparam VER_BLANK_START = 900;
+localparam VER_BLANK_TIME  = 26;
 
-localparam HBLNK_START_BACK = 0;
-localparam HBLNK_STOP_BACK = 0;
+localparam VER_SYNC_START = 903;
+localparam VER_SYNC_TIME  = 32;
 
-localparam HSYNC_START = 1487;
-localparam HSYNC_STOP = 1518;
+// ============================================================
+// Internal calcuations
 
+localparam HCOUNT_MAX = HOR_TOTAL_TIME - 1;
+localparam VCOUNT_MAX = VER_TOTAL_TIME - 1;
 
+localparam HBLNK_START_FRONT = HOR_BLANK_START - 1;
+localparam HBLNK_STOP_FRONT  = HBLNK_START_FRONT + HOR_BLANK_TIME -1;
 
-localparam VBLNK_START_BACK = 0;
-localparam VBLNK_STOP_BACK = 16;
+localparam HSYNC_START = HOR_SYNC_START - 1;
+localparam HSYNC_STOP  = HSYNC_START + HOR_SYNC_TIME - 1;
 
-localparam VBLNK_START_FRONT = 900;
-localparam VBLNK_STOP_FRONT = 925;
+localparam VBLNK_START_FRONT = VER_BLANK_START - 1;
+localparam VBLNK_STOP_FRONT  = VBLNK_START_FRONT + VER_BLANK_TIME - 1;
 
-
-localparam VSYNC_START = 902;
-localparam VSYNC_STOP = 907;
-
-
-
-// Add VGA timing parameters here and refer to them in other modules.
+localparam VSYNC_START = VER_SYNC_START - 1;
+localparam VSYNC_STOP  = VSYNC_START + VER_SYNC_TIME - 1;
 
 endpackage
